899bbe47863b1abf4ed9a4e2513b4de0e8f4c67d
Sajan Chouhan <sajan.chouhan@happay.in>
2023-05-29 22:57:45 +0530

    changes done

--- src/Component/Header/index.js
+++ src/Component/Header/index.js
@@ -21,13 +21,13 @@ export default function NewHeader({ className }) {
             <a href="/">Home </a>
           </li>
           <li>
-            <a href="/about">About </a>
+            <a href="about">About </a>
           </li>
           <li>
-            <a href="/blogs">Blogs </a>
+            <a href="blogs">Blogs </a>
           </li>
           <li>
-            <a href="/contact">Contact </a>
+            <a href="contact">Contact </a>
           </li>
         </ul>
 
