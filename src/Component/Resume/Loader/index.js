8519696226b56235d1fbdee24144b7ce5c28b5de
Sajan Chouhan <sajan.chouhan@happay.in>
2024-02-10 22:49:16 +0530

    resume made conpatible with app script

---
+++ src/Component/Resume/Loader/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Lottie from "react-lottie";
+import * as animationData from "../UIR9aOeQIZ.json";
+
+export default function Loader() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <div className="lottie-loader">
+      <Lottie
+        options={defaultOptions}
+        height={600}
+        width={600}
+        isStopped={false}
+        isPaused={false}
+      />
+    </div>
+  );
+}
